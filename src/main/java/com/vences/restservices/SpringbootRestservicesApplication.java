f6b24bb9910462777e9fb32e678d66b6d2f16482
David Vences <dvences90@gmail.com>
2020-09-01 11:42:42 -0500

    First Commit

---
+++ src/main/java/com/vences/restservices/SpringbootRestservicesApplication.java
@@ -0,0 +1,13 @@
+package com.vences.restservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootRestservicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vences/restservices/SpringbootRestservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vences.restservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
